f57c300933a06b2b31a95c20314503bd5a393cc0
pc <pc@DESKTOP-BI5FP4B>
2020-12-20 14:34:55 +0100

    Created MenuItem model, and added to the DB

--- Restaurant.DataAccess/ApplicationDbContext.cs
+++ Restaurant.DataAccess/ApplicationDbContext.cs
@@ -17,5 +17,7 @@ namespace Restaurant.DataAccess
         public DbSet<Category> Category { get; set; }
 
         public DbSet<FoodType> FoodType { get; set; }
+
+        public DbSet<MenuItem> MenuItem { get; set; }
     }
 }
