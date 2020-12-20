4b2d780a4bfb55b8245e93d9bb7013a82c17b6b8
pc <pc@DESKTOP-BI5FP4B>
2020-12-20 10:57:13 +0100

    Completed all the implementations for the repository methods

--- Restaurant.DataAccess/Data/Repository/Repository.cs
+++ Restaurant.DataAccess/Data/Repository/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Restaurant.DataAccess.Data.Repository.IRepository;
@@ -31,12 +32,51 @@ namespace Restaurant.DataAccess.Data.Repository
 
         public IEnumerable<T> GetAll(System.Linq.Expressions.Expression<Func<T, bool>> filter = null, Func<System.Linq.IQueryable<T>, System.Linq.IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                //if there are several properties separated by commas it will add to the query variable
+                foreach (var includeProperty in includeProperties.Split(new char[] {','},
+                    StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+
+            return query.ToList();
         }
 
         public T GetFirstOrDefault(System.Linq.Expressions.Expression<Func<T, bool>> filter = null, string includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                //if there are several properties separated by commas it will add to the query variable
+                foreach (var includeProperty in includeProperties.Split(new char[] {','},
+                    StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return query.FirstOrDefault();
         }
 
         public void Remove(int id)
