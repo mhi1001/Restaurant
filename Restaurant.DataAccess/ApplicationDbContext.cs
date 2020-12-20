6b588096100a09b9d41bd3d4e4a982409b325165
pc <pc@DESKTOP-BI5FP4B>
2020-12-20 10:04:07 +0100

    Structured the solution and added references

--- Restaurant.DataAccess/ApplicationDbContext.cs
+++ Restaurant.DataAccess/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Restaurant.Data
+namespace Restaurant.DataAccess
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- Restaurant.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Restaurant.DataAccess
-{
-    public class Class1
-    {
-    }
-}
